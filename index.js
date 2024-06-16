549187d1e05da00c10579d17279a530e155baf38
Fábio <fabiojas@ufba.br>
2024-06-16 17:03:06 -0300

    feat: finalizado tratamento de erro

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs').promises
+
+async function readFile() {
+    try {
+        const data = await fs.readFile('textFile.txt', 'utf8');
+        let words = 0 
+        if(data !== ''){
+            const wordArray = data.trim().split(/\s+/)
+            words = wordArray.length
+            console.log("Aguarde a leitura do texto...")
+            return words
+            
+        } else{
+            console.log('O arquivo está vazio')
+            return 0
+        }
+    
+        
+    }
+    catch (err) {
+        console.log("Erro: Não foi possível realizar a leitura")
+        }
+    }
+
+async function showWordsCount() {
+    try {
+        const numWords = await readFile();
+        console.log(`Total de ${numWords} palavras presentes no texto`)
+    }
+    catch(err){
+        console.log(err)
+    }
+}
+
+showWordsCount()
